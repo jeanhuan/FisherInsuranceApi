8bc91d043cca9bbbee25cd1c1a01273113dd055c
jeanhuan <jeanhuan@gmail.com>
2017-05-02 21:06:29 -0400

    exercise-3

---
+++ wwwroot/app/components/claims/claims.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'claims-home-page',
+    templateurl: './app/components/claims/claims.component.html'
+})
+
+export class ClaimsComponent {
+    
+}
---
+++ wwwroot/app/components/quotes/quotes.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'quotes-home-page',
+    templateurl: './app/components/quotes/quotes.component.html'
+})
+
+export class QuotesComponent {
+    
+}
